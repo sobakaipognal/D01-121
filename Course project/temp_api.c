9bdffee1933932566646438021b403fe01f7647b
avseykov andrew <a.abc@mail.ru>
2026-04-15 23:39:14 +0300

    Course project

---
+++ Course project/temp_api.c
@@ -0,0 +1,31 @@
+#include "temp_api.h"
+
+void month_mid_T(int* data, int month, int year)
+{
+
+}
+
+void month_low_T(int* data, int month, int year)
+{
+    
+}
+
+void month_max_T(int* data, int month, int year)
+{
+    
+}
+
+void year_mid_T(int* data, int year)
+{
+    
+}
+
+void year_low_T(int* data, int year)
+{
+    
+}
+
+void year_max_T(int* data, int year)
+{
+    
+}

b1681a7164f68c1d9bd05587805316826a87ea56
sobakaipognal <a.abc@mail.ru>
2026-05-09 17:28:12 +0300

    Delete Course project directory

--- Course project/temp_api.h
+++
@@ -1,6 +0,0 @@
-void month_mid_T(int* data, int month, int year);
-void month_low_T(int* data, int month, int year);
-void month_max_T(int* data, int month, int year);
-void year_mid_T(int* data, int year);
-void year_low_T(int* data, int year);
-void year_max_T(int* data, int year);
